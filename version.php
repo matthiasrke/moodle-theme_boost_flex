6063960f2da8d9a40fd913a6bfd9edacab172f51
matthiasrke <info.reike@gmail.com>
2021-09-19 14:17:59 +0200

    Fixed version nummer in ''

--- version.php
+++ version.php
@@ -1,12 +1,38 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+/**
+ * Version file.
+ *
+ * @package    theme_boost_flex
+ * @copyright  2021 Matthias Reike
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2021080600';
+$plugin->version = 2021080600;
+
+// This is the named version.
+$plugin->release = 3.11.01;
+
+// This is a stable release.
+$plugin->maturity = MATURITY_STABLE;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = '2020110300';
@@ -17,5 +43,5 @@ $plugin->component = 'theme_boost_flex';
 
 // This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
-    'theme_boost' => '2020110900'
+    'theme_boost' => 2020110900
 ];
